c1e63c2efebd5de0d54dd5cce6c2f0fade46f428
Nick71100 <nick.delattre4@gmail.com>
2025-04-02 18:56:20 +0200

    commit

--- app.js
+++ app.js
@@ -6,3 +6,35 @@ Le modal disparait lorsqu'on recommence le cycle.
 Un deuxième bouton doit servir à masquer le modal.
 
 */
+const moveDiv = document.getElementById("moveDiv");
+const hideModal = document.getElementById("hideModal");
+const container = document.querySelector(".container");
+const red = document.querySelector(".red");
+const modal = document.querySelector(".modal");
+
+let position = 0;
+
+moveDiv.addEventListener("click", function () {
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+  const redWidth = red.clientWidth;
+  const redHeight = red.clientHeight;
+
+  if (position === 0) {
+    red.style.left = `${containerWidth - redWidth}px`;
+    modal.style.display = "none";
+  } else if (position === 1) {
+    red.style.top = `${containerHeight - redHeight}px`;
+  } else if (position === 2) {
+    red.style.left = `0px`;
+  } else {
+    red.style.top = `0px`;
+    modal.style.display = "block";
+  }
+
+  position = (position + 1) % 4;
+});
+
+hideModal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
